bdfeb51b9521f82f933397a82f88a90c33440011
Wesley Paiva <wesley.paiva@euemdia.com.br>
2021-08-03 22:26:29 -0300

    Adicionando alteração da branch mod_01

--- voce-em-dia/src/App.js
+++ voce-em-dia/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Texto editado pra dar visible pra a branch mod_01
+          Texto editado pra dar visible pra a branch mod_01.
         </p>
         <a
           className="App-link"
