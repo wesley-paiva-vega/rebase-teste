024500125aeb20d42c2399e3ee7eabe1c208215a
wesley-snybool-dev <wesley.snybool.dev@gmail.com>
2021-08-03 22:42:27 -0300

    altered carbon

--- voce-em-dia/src/App.js
+++ voce-em-dia/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Add diretamente da branch master
         </a>
       </header>
     </div>
